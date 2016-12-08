6465e10253cc9bc4b858e35d3de8cad87e4855fe
mixkorea <mixkorea@WIN-0K7GMNJ4HG7>
2016-12-08 13:22:00 +0900

    아이템수정

--- life_client/config/Config_Clothing.hpp
+++ life_client/config/Config_Clothing.hpp
@@ -780,8 +780,8 @@ class Clothing {
             { "H_Shemag_olive", "", 48500, { "", "", -1 } },
             { "H_MilCap_oucamo", "", 12000, { "", "", -1 } },
             { "H_HelmetO_ocamo", "", 25000, { "", "", -1 } },
-            { "H_CrewHelmetHeli_I", "Gas Mask", 1000000, { "", "", -1 } },
-            { "H_HelmetO_ViperSP_hex_F", "특수목적hex[1회]", 990000, { "", "", -1 } }
+            { "H_CrewHelmetHeli_I", "Gas Mask", 990000, { "", "", -1 } }
+            //{ "H_HelmetO_ViperSP_hex_F", "특수목적hex[1회]", 990000, { "", "", -1 } }
         };
         goggles[] = {
             { "NONE", "Remove Glasses", 0, { "", "", -1 } },
